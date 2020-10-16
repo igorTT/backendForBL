63f7a82207de6b667c9ba0cf62e778ce27900758
IgorTT <hirzhek@gmail.com>
2020-10-16 17:33:31 +0300

    backend for bl
    add bootstrap
    add env support

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { config } from 'dotenv';
+
+config();
+
+const PORT = process.env.PORT || 5000;
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(PORT);
+}
+bootstrap();
