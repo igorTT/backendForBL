38852867b5aa638474bf207db7fa7d6c5dc9cb3b
IgorTT <hirzhek@gmail.com>
2020-10-22 17:43:39 +0300

    minor
    add integrations controller

--- src/integrations/integrations.module.ts
+++ src/integrations/integrations.module.ts
@@ -3,9 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Integrations } from './integrations.entity';
 import { IntegrationsResolver } from './integrations.resolver';
 import { IntegrationsService } from './integrations.service';
+import { IntegrationsController } from './integrations.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Integrations])],
   providers: [IntegrationsResolver, IntegrationsService],
+  controllers: [IntegrationsController],
 })
 export class IntegrationsModule {}
