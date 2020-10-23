07183a07e5d42434c7830d73ad9a8eacef259c06
IgorTT <hirzhek@gmail.com>
2020-10-23 17:51:06 +0300

    fix

--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -1,13 +1,14 @@
 import * as bcrypt from 'bcrypt';
 import { AuthCredentialsDto } from 'src/auth/dto/auth-credentials.dto';
 import { EntityRepository, Repository } from 'typeorm';
+import { CreateUserInput } from './create-user-input';
 
 import { User } from './user.entity';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
-  async signUp(authCredentialsDto: AuthCredentialsDto) {
-    const { email, password, username } = authCredentialsDto;
+  async signUp(createUserInput: CreateUserInput) {
+    const { email, password, username } = createUserInput;
   }
 
   private async hashPassword(password: string, salt: string): Promise<string> {
